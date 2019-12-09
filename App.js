c2b638b785fae315bc9e7c8180bc60fd0d5d373f
João Melo <jopcmelo@gmail.com>
2019-12-09 18:37:01 -0300

    chore: Add a basic After Effects animation with Lottie

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import Lottie from 'lottie-react-native';
+
+import rocket from './rocket.json';
+
+export default function App() {
+  return (
+    <SafeAreaView
+      style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Lottie resizeMode="contain" source={rocket} autoPlay loop autoSize />
+    </SafeAreaView>
+  );
+}
